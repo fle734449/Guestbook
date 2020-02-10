1ef67662fd34892fea63cb641b983bb74b174bdb
fle734449 <36311780+fle734449@users.noreply.github.com>
2020-02-10 17:35:21 -0600

    Reverted back to where pressing subscribe sends email. Still haven't gotten cron to work yet

--- src/main/java/guestbook/SendEmail.java
+++ src/main/java/guestbook/SendEmail.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Logger;
+import javax.servlet.annotation.WebFilter;
+import com.googlecode.objectify.ObjectifyFilter;
 
 import javax.mail.Message;
 import javax.mail.Session;
@@ -14,6 +16,8 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
+
+import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyService;
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
@@ -31,47 +35,7 @@ import com.google.appengine.api.users.UserServiceFactory;
 public class SendEmail extends HttpServlet {
 	private static final Logger _logger = Logger.getLogger(SendEmail.class.getName());
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		_logger.info("Cron Job has been executed");
-		ObjectifyService.register(Subscribe.class);
-        UserService userService = UserServiceFactory.getUserService();
-        User user = userService.getCurrentUser();
 
-        String guestbookName = req.getParameter("guestbookName");
-        Key guestbookKey = KeyFactory.createKey("Guestbook", guestbookName);
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        List<Subscribe> s = ObjectifyService.ofy().load().type(Subscribe.class).list();
-	    List<String> subscribers = s.get(0).getSubscribers();
-	    
-        Query query = new Query("Greeting", guestbookKey).addSort("date", Query.SortDirection.DESCENDING);
-		List<Entity> posts = datastore.prepare(query).asList(FetchOptions.Builder.withDefaults());
-		
-		Collections.reverse(posts);
-		
-		String blogPosts = "";
-		
-		for(Entity post: posts) {
-			if(inLastDay((Date) post.getProperty("date"))) {
-				blogPosts +=  post.getProperty("title") + "\n" + post.getProperty("user") + "\n" + post.getProperty("date") + "\n" + post.getProperty("content")  + "\n" + "\n";
-			}
-		}
-		try {
-			if(!(blogPosts.equals(""))) {
-				for (String sub: subscribers) {
-					_logger.info("Email has been sent to" + sub);
-					Properties p = new Properties();
-				    Session session = Session.getDefaultInstance(p, null);
-				    MimeMessage msg = new MimeMessage(session);
-				    msg.setFrom(new InternetAddress("fle734449@gmail.com"));
-				    msg.addRecipient(Message.RecipientType.TO,new InternetAddress(sub));
-				    msg.setSubject("You have subscribed to ThoughtBubbles");
-				    msg.setText("Dear Subscriber, \nHere are the blog posts made in the last 24 hours:\n\n" + blogPosts);
-				    Transport.send(msg);
-				}
-			}
-		} catch (Exception e) {
-			
-		}
-		
 		
 		
         /*
@@ -93,9 +57,12 @@ public class SendEmail extends HttpServlet {
 	doGet(req, resp);
 	}
 	
-	public final static long MILLIS_PER_DAY = 24 * 60 * 60 * 1000;
-	public boolean inLastDay(Date date) {
-		return date.getTime() > System.currentTimeMillis() - MILLIS_PER_DAY;
-	}
+	
+	
+
+/*
+	@WebFilter(urlPatterns = {"/*"})
+	public class ObjectifyWebFilter extends ObjectifyFilter {}
+	*/
 	
 }
--- src/main/java/guestbook/Subscribe.java
+++ src/main/java/guestbook/Subscribe.java
@@ -19,7 +19,6 @@ import javax.servlet.http.*;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
@@ -27,11 +26,17 @@ import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+import com.googlecode.objectify.annotation.Parent;
+
 
 public class Subscribe extends HttpServlet {
+	@Id long id;
 	private static final Logger _logger = Logger.getLogger(Subscribe.class.getName());
-	List<String> subscribers = new ArrayList<String>();
-	String subEmail;
+	@Index List<String> subscribers = new ArrayList<String>();
+	@Index String subEmail;
 	
 	public Subscribe(){}
 
@@ -44,6 +49,7 @@ public class Subscribe extends HttpServlet {
 		return subscribers;
 	}
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
 		UserService userService = UserServiceFactory.getUserService();
         User user = userService.getCurrentUser();    
 		String email = user.getEmail();
@@ -52,11 +58,46 @@ public class Subscribe extends HttpServlet {
 	        		subscribers.add(email);
 	        	}
 	        }
+	        String guestbookName = req.getParameter("guestbookName");
+	        Key guestbookKey = KeyFactory.createKey("Guestbook", guestbookName);
+	        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		    
+	        Query query = new Query("Greeting", guestbookKey).addSort("date", Query.SortDirection.DESCENDING);
+			List<Entity> posts = datastore.prepare(query).asList(FetchOptions.Builder.withDefaults());
+			
+			Collections.reverse(posts);
+			
+			String blogPosts = "";
+			
+			for(Entity post: posts) {
+				if(inLastDay((Date) post.getProperty("date"))) {
+					blogPosts +=  post.getProperty("title") + "\nBy:" + post.getProperty("user") + "\nPosted on:" + post.getProperty("date") + "\n" + post.getProperty("content")  + "\n" + "\n";
+				}
+			}
+			try {
+				if(!(blogPosts.equals(""))) {
+					for (String sub: subscribers) {
+						_logger.info("Email has been sent to" + sub);
+						Properties p = new Properties();
+					    Session session = Session.getDefaultInstance(p, null);
+					    MimeMessage msg = new MimeMessage(session);
+					    msg.setFrom(new InternetAddress("fle734449@gmail.com"));
+					    msg.addRecipient(Message.RecipientType.TO,new InternetAddress(sub));
+					    msg.setSubject("You have subscribed to ThoughtBubbles");
+					    msg.setText("Dear Subscriber, \nHere are the blog posts made in the last 24 hours:\n\n" + blogPosts);
+					    Transport.send(msg);
+					}
+				}
+			} catch (Exception e) {
+				
 	        
-	        resp.sendRedirect("/guestbook.jsp");
-	        
-	        
-
-	        
+			}
+			resp.sendRedirect("/guestbook.jsp");
+			
+	
+		}
+	public final static long MILLIS_PER_DAY = 24 * 60 * 60 * 1000;
+	public boolean inLastDay(Date date) {
+		return date.getTime() > System.currentTimeMillis() - MILLIS_PER_DAY;
 	}
 }
